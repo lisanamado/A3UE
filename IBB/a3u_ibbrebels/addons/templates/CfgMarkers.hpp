a6c1f500aee17cd61fc8f733465ac29025d406c5
Lisan Amado <lisandroamado@gmail.com>
2024-11-15 19:51:41 -0300

    Actualizar CfgMarkers.hpp

--- IBB/a3u_ibbrebels/addons/templates/CfgMarkers.hpp
+++ IBB/a3u_ibbrebels/addons/templates/CfgMarkers.hpp
@@ -4,8 +4,8 @@ class CfgMarkers
 
 	class my_extension_marker_aaf_remnants: flag_NATO 
 	{
-		name = "AAF Remnants";
-		icon = QPATHTOFOLDER(Pictures\Markers\marker_aaf_torn_co.paa);
-		texture = QPATHTOFOLDER(Pictures\Markers\marker_aaf_torn_co.paa);
+		name = "IBB";
+		icon = QPATHTOFOLDER(Templates\IBB\IBB.paa);
+		texture = QPATHTOFOLDER(Templates\IBB\IBB.paa);
 	};
 };
