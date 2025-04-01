b96fe6d2af30047e57ca5a5cc008bb6297641810
Lisan Amado <lisandroamado@gmail.com>
2025-04-01 09:29:11 -0300

    Comienzo de NARCOS

---
+++ NARCOS/a3ue_narcos/addons/functions/script_component.hpp
@@ -0,0 +1,2 @@
+#define COMPONENT functions
+#include "\x\a3ue_narcos\addons\core\Includes\script_mod.hpp"
---
+++ NARCOS/a3ue_narcos/addons/maps/script_component.hpp
@@ -0,0 +1,2 @@
+#define COMPONENT maps
+#include "\x\a3ue_narcos\addons\core\Includes\script_mod.hpp"
